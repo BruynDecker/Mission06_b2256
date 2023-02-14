0ef536c392f56532004ca09bdc6108000ba643ca
bruyndecker <bruyn.decker@byu.edu>
2023-02-13 22:38:19 -0700

    Add project files.

---
+++ Mission06_b2256/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_b2256.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_b2256.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private EnterFilmContext blahContext { get; set; }
+
+
+        public HomeController(ILogger<HomeController> logger, EnterFilmContext somename)
+        {
+            _logger = logger;
+            blahContext = somename;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("Podcasts");
+        }
+
+        [HttpGet]
+        public IActionResult EnterFilm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EnterFilm(ApplicationResponse ar)
+        {
+            blahContext.Add(ar);
+            blahContext.SaveChanges();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_b2256/Models/EnterFilmContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission06_b2256.Models
+{
+    public class EnterFilmContext : DbContext
+    {
+        public EnterFilmContext(DbContextOptions<EnterFilmContext> options) : base(options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<ApplicationResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+                new ApplicationResponse
+                {
+                    ApplicationID = 1,
+                    Category = "Family",
+                    Title = "Nim's Island",
+                    Year = 2006,
+                    Director = "Brendan Maher, Mark Levin, Jennifer Flackett",
+                    Rating = "PG",
+                    Edited = false
+                },
+                new ApplicationResponse
+                {
+                    ApplicationID = 2,
+                    Category = "Miscellaneous",
+                    Title = "Star Wars: Rise of the Assassins",
+                    Year = 2014,
+                    Director = "Bruyn Decker, Luke Maas, Spencer Lyman",
+                    Rating = "NR",
+                    LentTo = "Bruyn's Parents",
+                    Notes = "A silly homemade film I [Bruyn] made with my cousins at a family reunion"
+                },
+                new ApplicationResponse
+                {
+                    ApplicationID = 3,
+                    Category = "Action/Adventure",
+                    Title = "Casino Royale",
+                    Year = 2006,
+                    Director = "Martin Campbell",
+                    Rating = "PG-13",
+                    Notes = "The best Bond Film"
+                }
+            ); ; ;
+        }
+    }
+}
